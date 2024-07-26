be3984e50478098455fe2ecc0a717de70ff5a941
Hannah Greene <questor96@gmail.com>
2024-07-26 15:43:37 -0500

    remove intermediary js, compile to subdirectory

--- common/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CardRequestBody {
-    cardNum: number;
-    constructor(cardNum: number);
-}
-export declare class CardResponseBody {
-    response: boolean;
-    constructor(response: boolean);
-}
